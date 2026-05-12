e7a364891952d0e455654aeff6f6efcc6a736ffa
Muhammad Maaz Sheikh <maazsheikh786@hotmail.com>
2026-05-12 11:58:19 +0500

    Change app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello from CI/CD Pipeline! Updated text 🚀\n');
+    res.end('Hello from CI/CD Pipeline!\n');
 });
 
 server.listen(3000, () => {
